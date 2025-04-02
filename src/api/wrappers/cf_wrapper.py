c40da9823c3ff9530194952e2935a821c42a1429
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-04-01 22:38:26 -0400

    feat: add error handling for CurseForge API wrapper using FireErrors
    feat: explicitly indicate FireErrors are direct consequence of thrown errors with 'from err'
    
    feat: suppress errors for CurseForge wrapper and edit error messages under the context of them being user-facing

--- src/api/wrappers/cf_wrapper.py
+++ src/api/wrappers/cf_wrapper.py
@@ -1,20 +1,21 @@
+import fire
 import requests
-
-from src.api.dataclasses.cf_response_objects import CFGetModResponse, CFGetModData
+from json.decoder import JSONDecodeError
+from src.api.dataclasses.cf_response_objects import CFGetModResponse, CFGetModData, CFDataResponse
 
 
 class CurseForgeWrapper:
     """Wrapper for the CurseForge API."""
 
-    def __init__(self, api_key):
-        self.api_key = api_key
+  def __init__(self, api_key):
+    self.api_key = api_key
 
     def get_mod(self, mod_id: int) -> CFGetModData:
         """
         Return CFGetModData object containing mod metadata
 
-        Parameters:
-        mod_id (int): The Mod ID for the mod metadata to be fetched
+    Parameters:
+    mod_id (int): The Mod ID for the mod metadata to be fetched
 
         Returns:
         CFGetModData: Object containing mod metadata
@@ -25,7 +26,22 @@ class CurseForgeWrapper:
             'x-api-key': self.api_key
         }
 
-        response = requests.get(get_mod_url, headers=headers)
-        mod_data = CFGetModResponse(**response.json())
+    try:
+      response = requests.get(get_mod_url, headers=headers)
+    except requests.exceptions.HTTPError as err:
+      raise FireError(
+        f'A problem occurred while querying the CurseForge API for mod {mod_id}') from err
+    except requests.exceptions.RequestException as err:
+      raise FireError(
+        f'A problem occurred while querying the CurseForge API for mod {mod_id}') from err
 
-        return mod_data.data
+    try:
+      mod_data = CFGetModResponse(**response.json())
+    except JSONDecodeError as err:
+      raise FireError(
+        f'Failed to decode JSON payload for mod {mod_id}') from err
+    except TypeError as err:
+      raise FireError(
+        f'Failed to process API response for mod {mod_id}') from err
+
+    return CFDataResponse(payload=mod_data, statusCode=response.status_code, status=response.reason)
