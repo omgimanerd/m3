6725c60bc467fbd84fc550414583b2ef3c4e7037
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-04-26 23:22:18 -0400

    Hide the multikey implementation from user

--- src/lib/multikey_dict.py
+++ src/lib/multikey_dict.py
@@ -1,22 +1,22 @@
 """Class for multikey dicts."""
 
-from typing import Hashable, Union
+from typing import Hashable
 
 
 class MultiKeyDict:
     """Utility class that handles mapping multiple keys to the same value.
 
-    Values are keyed by a tuple of keys (a multikey), of which any can be used to 
-    look up the value. The size of the tuple is user-specified, fixed, and must 
-    be known at instantiation time. Adding values to the dict requires a 
+    Values are keyed by a tuple of keys (a multikey), of which any can be used
+    to look up the value. The size of the tuple is user-specified, fixed, and
+    must be known at instantiation time. Adding values to the dict requires a
     complete tuple of keys, but lookups can be done with any key in the tuple.
 
-    Note that when removing a value, the associated multikey will also be removed.
-    Removal or modification of individual keys after a value is added is not 
-    supported.
+    Note that when removing a value, the associated multikey will also be
+    removed. Removal or modification of individual keys after a value is added
+    is not supported.
 
-    It may be useful to stick to consistent ordering of these keys for all data 
-    entries when adding values, but the ordering must be enforced by the caller 
+    It may be useful to stick to consistent ordering of these keys for all data
+    entries when adding values, but the ordering must be enforced by the caller
     of this class.
     """
 
@@ -26,43 +26,40 @@ class MultiKeyDict:
         self.keys_to_multikeys = {}
         self.data = {}
 
-    def _validate_multikey(self, multikey: tuple) -> bool:
-        """Validates that a given multikey contains the right number of keys, 
-        each key is a valid key for the dict, and the multikey is linked to data.
+    def _validate_multikey(self, multikey: tuple):
+        """Validates that a given multikey contains the right number of keys.
 
         Returns true if the multikey is valid, raises error otherwise.
         """
         if len(multikey) != self.num_of_keys:
             raise ValueError(
                 f'Expected {self.num_of_keys} keys, got {len(multikey)}')
-        for key_ in multikey:
-            if not self.is_existing_key(key_):
-                raise ValueError(f'Given key {key_} was not found')
-        if multikey not in self.data:
-            raise ValueError(
-                f'Given multikey {multikey} not associated with data')
-        return True
+
+    def __getattribute__(self, multikey: tuple) -> any:
+        pass
 
     def is_existing_key(self, key: Hashable) -> bool:
-        """Returns True if a given key is an existing individual key for the 
-        dict, else returns False."""
+        """Returns True if the given key is an existing key in the 
+        key_to_multikey map."""
         return key in self.keys_to_multikeys
 
+    def is_existing_multikey(self, key: tuple) -> bool:
+        """Returns True if a given multikey is an existing multikey for the 
+        dict."""
+        return key in self.data
+
     def add(self, multikey: tuple, data: any):
-        """Given a multikey and data, adds the entry to the dict. The multikey 
+        """Given a multikey and data, adds the data to the dict. The multikey
         must contain the number of expected keys.
 
         Args:
-            multikey: the multikey to use to index the data
-            data: the data to add to dict
+            multikey: the multikey to use to index the data data: the data to
+            add to dict
         """
-        if len(multikey) != self.num_of_keys:
-            raise ValueError(
-                f'Expected {self.num_of_keys} keys, got {len(multikey)}')
+        self._validate_multikey(multikey)
         for key_ in multikey:
             if self.is_existing_key(key_):
                 raise ValueError(f'Given key {key_} already exists')
-        for key_ in multikey:
             self.keys_to_multikeys[key_] = multikey
         self.data[multikey] = data
 
@@ -73,36 +70,28 @@ class MultiKeyDict:
             multikey: the multikey to remove from dict
 
         Returns:
-            The removed multikey as a tuple, else raise an error.
+            The removed data entry, else raise an error.
         """
         self._validate_multikey(multikey)
         for key_ in multikey:
             self.keys_to_multikeys.pop(key_)
         return self.data.pop(multikey)
 
-    def get(self, key: Union[tuple, Hashable]) -> any:
-        """Given a key or multikey, gets the associated data.
+    def get(self, key: Hashable) -> any:
+        """Given a key that is part of an existing multikey, gets the associated
+         data.
 
         Args:
-            key: the key or multikey to look up the data with
+            key: the key to look up the data with
 
         Returns:
             The data associated with the given key or None.
         """
-        if isinstance(key, tuple):
-            self._validate_multikey(key)
-            return self.data[key]
-        if isinstance(key, Hashable):
-            if self.is_existing_key(key):
-                return self.data[self.keys_to_multikeys[key]]
-        return None
+        return self.data.get(self.keys
+                             .get(key, None), None)
 
     def len(self) -> int:
-        """Returns the number of multikeys mapped to data.
-
-        Note that each multikey is counted as one despite potentially containing 
-        multiple keys.
-        """
+        """Returns the number of data entries."""
         return len(self.data)
 
     def get_multikeys(self):
