b93badb2c9bf34792f560b69c3ef326fbb2eeb30
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-04-27 03:36:00 -0400

    Change download_file utility function to take destination as Path, set to overwrite file if it exists

--- src/util/web_util.py
+++ src/util/web_util.py
@@ -5,16 +5,15 @@ from pathlib import Path
 import requests
 
 
-def download_file(url: str, filename: str, dest: str):
+def download_file(url: str, dest: Path):
     """Download asset file from given CDN link.
 
     Args:
         url: The CDN link to download asset file from
-        filename: The name of the file to write once it is downloaded
         dest: The destination path to download to
     """
     response = requests.get(url, timeout=10)
-    path = Path(dest) / filename
+    path = Path(dest)
 
-    with open(path, 'wb') as f:
+    with open(path, 'wb+') as f:
         f.write(response.content)
