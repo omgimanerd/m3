3e4213b8f98e864373255fbd8ce9f2ed7aed2c51
omgimanerd <alvin@omgimanerd.tech>
2025-05-07 21:57:11 -0400

    Refactor command alias functionality into custom AliasGroup and AliasCommand subclasses.

--- src/cli/apply.py
+++ src/cli/apply.py
@@ -4,9 +4,8 @@ import click
 
 from src.config.config import Config
 from src.config.lockfile import Lockfile
-from src.config.lockfile_entry_manager import LockfileEntryManager
 
-HELPTEXT = """
+R_HELPTEXT = """
 Remove assets found in the project asset directories that are not in the
 lockfile.
 """
@@ -16,19 +15,17 @@ lockfile.
 class Apply:
     """Class for the apply command."""
     @click.command()
-    @click.option('-r', '--remove', is_flag=True, help=HELPTEXT)
+    @click.option('-r', '--remove', is_flag=True, help=R_HELPTEXT)
     @staticmethod
     def apply(remove):
-        """Installs assets recorded in the lockfile into the project asset
-        directories.
-        """
+        """Applies the lockfile's state to the project assets."""
         config = Config.get_config()
         lockfile = Lockfile.create(config.get_path())
-        lockfile_entry_manager = LockfileEntryManager(lockfile, config)
-        lockfile_entry_manager.config_hash_maps()
-        lockfile_entry_manager.apply()
+        # lockfile_entry_manager = LockfileEntryManager(lockfile, config)
+        # lockfile_entry_manager.config_hash_maps()
+        # lockfile_entry_manager.apply()
 
-        if remove:
-            lockfile_entry_manager.prune()
-            click.echo('Pruned extraneous assets not found in lockfile')
-        click.echo('Applied lockfile state to development directory')
+        # if remove:
+        #     lockfile_entry_manager.prune()
+        #     click.echo('Pruned extraneous assets not found in lockfile')
+        # click.echo('Applied lockfile state to development directory')
