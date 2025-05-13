6637c1886187c00f5df40656c959dcdae657d300
omgimanerd <alvin@omgimanerd.tech>
2025-05-13 19:31:37 -0400

    Stub out test for reading config from path

--- src/config/config_test.py
+++ src/config/config_test.py
@@ -1,19 +1,29 @@
 """Unit testing for config.py"""
 
 import json
+import os
+from pathlib import Path
+from typing import Callable
 
 import pytest
 
-from src.config.config import CONFIG_FILENAME, Config
+from src.config.config import CONFIG_FILENAME, Config, ProjectPaths
 from src.util.enum import Platform
 
 
 @pytest.fixture
-def config(tmp_path):
-    pass
+def cwd() -> Path:
+    return Path(os.getcwd())
 
 
-# TODO: Make tests run pre-commit. CI?
+@pytest.fixture
+def config_from_path() -> Callable[[Path], Config]:
+    def _config_from_path(path):
+        with open(path, 'r', encoding='utf-8') as f:
+            return Config(**json.load(f), _path=path)
+    return _config_from_path
+
+
 def test_config_creation(tmp_path):
     c = Config(name="test-config", version="test")
     # TODO: make a fixture for config testing
@@ -43,5 +53,31 @@ def test_config_creation(tmp_path):
     }
 
 
-def test_config_read(tmp_path):
-    pass
+def test_config_read(cwd, config_from_path):
+    c = config_from_path(cwd / 'testdata/test_m3.json')
+    assert c == Config(
+        name="test-config",
+        version="test",
+        platform="curseforge",
+        authors=["test-author", "test-author2"],
+        paths=ProjectPaths(
+            mods="mods",
+            resourcepacks="custom_subdirectory/resourcepacks",
+            texturepacks="texturepacks",
+            shaderpacks="shaderpacks",
+        ),
+        output="output",
+        client_includes=[
+            Path("path/to/include_client")
+        ],
+        client_excludes=[
+            Path("path/to/exclude_client")
+        ],
+        server_includes=[
+            Path("path/to/include_server")
+        ],
+        server_excludes=[
+            Path("path/to/exclude_server")
+        ],
+        _path=cwd / 'testdata/test_m3.json'
+    )
