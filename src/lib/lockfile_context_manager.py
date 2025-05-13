b73030fbe220e0a494be44ca32b5efdc026f5b15
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-05-12 21:51:15 -0400

    Add context manager for m3's state on disk

--- src/lib/lockfile_context_manager.py
+++ src/lib/lockfile_context_manager.py
@@ -1,4 +1,30 @@
-"""File Context Manager for m3 lockfile."""
+"""Context Managers for m3's lockfile and state on disk."""
+
+from typing import Optional
+
+from src.config.config import Config
+from src.config.loader import load_config_and_lockfile
+from src.config.lockfile import Lockfile
+
+
+class M3ContextManager():
+    """Context manager for m3's state on disk."""
+
+    def __init__(self, config: Config, lockfile: Lockfile):
+        self.config = config
+        self.lockfile = lockfile
+
+    @staticmethod
+    def create():
+        config, lockfile = load_config_and_lockfile()
+        return M3ContextManager(config, lockfile)
+
+    def __enter__(self) -> tuple[Optional[Config], Optional[Lockfile]]:
+        return (self.config, self.lockfile)
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.config.write()
+        self.lockfile.write()
 
 
 class LockfileContextManager():
