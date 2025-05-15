7e85632d5b0d436c6b97b72b5c0a2a23b0677a37
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-05-14 20:34:28 -0400

    Remove redundant context manager for lockfile, fix linting

--- src/lib/lockfile_context_manager.py
+++ src/lib/lockfile_context_manager.py
@@ -15,6 +15,7 @@ class M3ContextManager():
         self.lockfile = lockfile
 
     @staticmethod
+    # pylint: disable-next=missing-function-docstring
     def create():
         config, lockfile = load_config_and_lockfile()
         return M3ContextManager(config, lockfile)
@@ -25,19 +26,3 @@ class M3ContextManager():
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.config.write()
         self.lockfile.write()
-
-
-class LockfileContextManager():
-    """Context manager for m3 lockfile."""
-
-    def __init__(self, filename: str, mode: str):
-        self.filename = filename
-        self.mode = mode
-        self.file = None
-
-    def __enter__(self):
-        self.file = open(self.filename, self.mode, encoding='utf-8')
-        return self.file
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.file.close()
