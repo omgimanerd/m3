cf2410a27ffaa85e5709d5ab4ce487e650b6e5ac
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-05-18 02:27:43 -0400

    Add working tests for lockfile, add method to access path

--- src/config/lockfile_test.py
+++ src/config/lockfile_test.py
@@ -2,8 +2,6 @@
 
 import json
 
-import pytest
-
 from src.config.lockfile import LOCKFILE_FILENAME, Lockfile
 from src.config.lockfile_entry import HashEntry, LockfileEntry
 from src.lib.asset import Asset
@@ -50,117 +48,83 @@ def test_lockfile_read(lockfile_from_path):
                     cdn_link="test-cdn-link"
                 )
             )
-        }
+        },
+        _path=l.get_path()
     )
 
 
-@pytest.mark.only
-def test_lockfile_serialize_deserialize(lockfile_from_path, tmp_path):
+def test_lockfile_write_read(lockfile_from_path, tmp_path):
     """Tests that writing a file to disk and reading that file results in the
     same lockfile object."""
     lockfile_path = tmp_path / LOCKFILE_FILENAME
-    # l = Lockfile(
-    #     entries={
-    #         "test-entry": LockfileEntry(
-    #             name="test-entry",
-    #             hash=HashEntry(
-    #                 sha1="sha1-hash",
-    #                 sha512="sha512-hash",
-    #                 md5="md5-hash"
-    #             ),
-    #             platform=Platform.CURSEFORGE,
-    #             asset_type=AssetType.MOD,
-    #             asset=Asset(
-    #                 name="test-mod",
-    #                 platform=Platform.CURSEFORGE,
-    #                 asset_type=AssetType.MOD,
-    #                 side=Side.BOTH,
-    #                 dependencies=[
-    #                     Asset(
-    #                         name="test-resource-pack",
-    #                         platform=Platform.CURSEFORGE,
-    #                         asset_type=AssetType.RESOURCE_PACK,
-    #                         side=Side.BOTH,
-    #                         dependencies=[],
-    #                         cdn_link="test-cdn-link-resource-pack"
-    #                     )
-    #                 ],
-    #                 cdn_link="test-cdn-link"
-    #             )
-    #         )
-    #     },
-    #     _path=lockfile_path
-    # )
-    le = LockfileEntry(
-        name="test-entry",
-        hash=HashEntry(
-            sha1="sha1-hash",
-            sha512="sha512-hash",
-            md5="md5-hash"
-        ),
-        platform=Platform.CURSEFORGE,
-        asset_type=AssetType.MOD,
-        asset=Asset(
-            name="test-mod",
-            platform=Platform.CURSEFORGE,
-            asset_type=AssetType.MOD,
-            side=Side.BOTH,
-            dependencies=[
-                Asset(
-                    name="test-resource-pack",
+    lockfile = Lockfile(
+        entries={
+            "test-entry": LockfileEntry(
+                name="test-entry",
+                hash=HashEntry(
+                    sha1="sha1-hash",
+                    sha512="sha512-hash",
+                    md5="md5-hash"
+                ),
+                platform=Platform.CURSEFORGE,
+                asset_type=AssetType.MOD,
+                asset=Asset(
+                    name="test-mod",
                     platform=Platform.CURSEFORGE,
-                    asset_type=AssetType.RESOURCE_PACK,
+                    asset_type=AssetType.MOD,
                     side=Side.BOTH,
-                    dependencies=[],
-                    cdn_link="test-cdn-link-resource-pack"
+                    dependencies=[
+                        Asset(
+                            name="test-resource-pack",
+                            platform=Platform.CURSEFORGE,
+                            asset_type=AssetType.RESOURCE_PACK,
+                            side=Side.BOTH,
+                            dependencies=[],
+                            cdn_link="test-cdn-link-resource-pack"
+                        )
+                    ],
+                    cdn_link="test-cdn-link"
                 )
-            ],
-            cdn_link="test-cdn-link"
-        )
+            )
+        },
+        _path=lockfile_path
     )
-    l = Lockfile(entries={"test-entry": le},  _path=lockfile_path)
-    l.write()
-    written_l = lockfile_from_path(lockfile_path)
-    print(written_l)
-    print(l)
-    print(le)
-    assert l == written_l
+    lockfile.write()
+    with open(lockfile_path, 'r', encoding='utf-8') as f:
+        written_json = json.load(f)
 
+    assert written_json == {
+        "entries": {
+            "test-entry": {
+                "name": "test-entry",
+                "hash": {
+                    "sha1": "sha1-hash",
+                    "sha512": "sha512-hash",
+                    "md5": "md5-hash"
+                },
+                "platform": "curseforge",
+                "asset_type": "mod",
+                "asset": {
+                    "name": "test-mod",
+                    "platform": "curseforge",
+                    "asset_type": "mod",
+                    "side": "both",
+                    "dependencies": [
+                        {
+                            "name": "test-resource-pack",
+                            "platform": "curseforge",
+                            "asset_type": "resource_pack",
+                            "side": "both",
+                            "dependencies": [],
+                            "cdn_link": "test-cdn-link-resource-pack"
+                        }
+                    ],
+                    "cdn_link": "test-cdn-link"
+                }
+            }
+        }
+    }
 
-def test_lockfile_entry_serializable(tmp_path, lockfile_entry_from_path):
-    lockfile_path = tmp_path / LOCKFILE_FILENAME
-    l = LockfileEntry(
-        name="test",
-        hash=HashEntry(sha1="sha1", sha512="sha512", md5="md5"),
-        platform=Platform.CURSEFORGE,
-        asset_type=AssetType.MOD,
-        asset=Asset(
-            name="test_asset",
-            platform=Platform.CURSEFORGE,
-            asset_type=AssetType.MOD,
-            side=Side.BOTH,
-            cdn_link="link",
-            dependencies=[]
-        )
-    )
-    with open(lockfile_path, 'w', encoding='utf-8') as f:
-        f.write(l.json())
-
-    written_l = lockfile_entry_from_path(lockfile_path)
-    print(written_l)
-
-
-def test_hash_entry_serializable(tmp_path):
-    lockfile_path = tmp_path / LOCKFILE_FILENAME
-    h = HashEntry(
-        sha1="sha1",
-        sha512="sha512",
-        md5="md5"
-    )
-    with open(lockfile_path, 'w', encoding='utf-8') as f:
-        f.write(h.json())
-
-
-def test_lockfile_write():
-    """Tests writing a lockfile to disk."""
-    pass
+    # Check that reading back the same lockfile results in the same object
+    written_lockfile = lockfile_from_path(lockfile_path)
+    assert lockfile == written_lockfile
