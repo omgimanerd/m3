3ba3005287a97481d01a0e297654a80d2d121692
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-06-03 18:39:02 -0400

    Update tests for hash utility functions to use direct comparison for MultiKeyDicts

--- src/util/hash_test.py
+++ src/util/hash_test.py
@@ -2,6 +2,7 @@
 
 
 from src.config.lockfile import HASH_ALGS
+from src.lib.multikey_dict import MultiKeyDict
 from src.util.enum import HashAlg
 from src.util.hash import hash_asset_dir, hash_asset_dir_multi_hash
 
@@ -28,31 +29,25 @@ def test_hash_asset_dir_multi_hash(current_dir, read_json_file):
     ref_hashes = read_json_file(
         current_dir / 'testdata/test_filename_to_hashes.json')
     multikey_dict = hash_asset_dir_multi_hash(asset_path, HASH_ALGS)
-    assert multikey_dict.len() == 3
-
-    multikey_dict_keys = multikey_dict.get_multikeys()
-    assert (
+    ref_dict = MultiKeyDict(4)
+    ref_dict.add((
         "a.jar",
         ref_hashes['md5']['a.jar'],
         ref_hashes['sha1']['a.jar'],
         ref_hashes['sha512']['a.jar'],
 
-    ) in multikey_dict_keys
-
-    assert (
+    ), asset_path / "a.jar")
+    ref_dict.add((
         "b.jar",
         ref_hashes['md5']['b.jar'],
         ref_hashes['sha1']['b.jar'],
         ref_hashes['sha512']['b.jar'],
-    ) in multikey_dict_keys
-
-    assert (
+    ), asset_path / "b.jar")
+    ref_dict.add((
         "c.zip",
         ref_hashes['md5']['c.zip'],
         ref_hashes['sha1']['c.zip'],
         ref_hashes['sha512']['c.zip'],
-    ) in multikey_dict_keys
+    ), asset_path / "c.zip")
 
-    assert multikey_dict.get("a.jar") == asset_path / "a.jar"
-    assert multikey_dict.get("b.jar") == asset_path / "b.jar"
-    assert multikey_dict.get("c.zip") == asset_path / "c.zip"
+    assert multikey_dict == ref_dict
