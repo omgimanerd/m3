32a2844468305a050d906e19f2cbb568dc3eb7d3
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-06-08 00:45:11 -0400

    Fix inconsistent test in copy_test by sorting the lists it asserts on

--- src/lib/copy_test.py
+++ src/lib/copy_test.py
@@ -8,10 +8,10 @@ from src.lib.copy import copy
 
 def get_files(root_dir: Path) -> list[str]:
     """Returns a list of all files in all subdirectories from the given root."""
-    return list(filter(
+    return sorted(list(filter(
         lambda path: (root_dir / path).is_file(),
         glob('**', root_dir=root_dir, recursive=True,
-             include_hidden=True)))
+             include_hidden=True))))
 
 
 def test_copy(current_dir, tmp_path):
@@ -26,7 +26,7 @@ def test_copy(current_dir, tmp_path):
         "**/jsconfig.json",
         "**/generator_script.py",
     ])
-    assert get_files(tmp_path) == [
+    assert get_files(tmp_path) == sorted([
         'config/config.json',
         'important_file',
         'kubejs/assets/asset1',
@@ -34,7 +34,7 @@ def test_copy(current_dir, tmp_path):
         'kubejs/assets/asset3',
         'kubejs/dev_script.js',
         'kubejs/script1.js',
-    ]
+    ])
 
 
 def test_copy2(current_dir, tmp_path):
@@ -49,9 +49,9 @@ def test_copy2(current_dir, tmp_path):
         "kubejs/assets/**",
         "**/dev*"
     ])
-    assert get_files(tmp_path) == [
+    assert get_files(tmp_path) == sorted([
         'config/config.json',
         'important_file',
         'kubejs/jsconfig.json',
         'kubejs/script1.js'
-    ]
+    ])
