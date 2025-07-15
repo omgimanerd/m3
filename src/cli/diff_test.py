caced26de57ac5ec0d441f66541499e6edac7b3d
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-07-14 21:48:16 -0400

    Implement tests for the diff CLI command using the utility function to build the output

--- src/cli/diff_test.py
+++ src/cli/diff_test.py
@@ -4,8 +4,7 @@ from unittest.mock import patch
 
 from click.testing import CliRunner
 
-from src.cli.diff import Diff
-from src.util.formatter import CustomOutputFormatter
+from src.cli.diff import Diff, DiffOutputBuilder
 
 
 @patch('src.config.lockfile.LOCKFILE_FILENAME', 'test_m3.lock.json')
@@ -19,19 +18,7 @@ def test_diff(copy_test_data_directory, current_dir, tmp_path):
     with runner.isolated_filesystem(temp_dir=tmp_path) as td:
         copy_test_data_directory(ref_path, td)
         result = runner.invoke(Diff.diff)
-    formatter = CustomOutputFormatter()
-    expected_output = formatter.format(
-        '{diff_title:title}', diff_title='Asset Diff')
-    expected_output += '\n' + formatter.format(
-        '{missing:header}', missing='Lockfile assets missing')
-    expected_output += '\n' + formatter.format(
-        '{missing_asset:diff_minus}', missing_asset='c.zip')
-    expected_output += '\n' + formatter.format(
-        '{separator:separator}', separator='')
-    expected_output += '\n' + formatter.format(
-        '{new_assets:header}', new_assets='New assets found')
-    new_asset_path = td + '/assets/mods/b.jar'
-    expected_output += '\n' + formatter.format(
-        '{new_asset:diff_plus}', new_asset=new_asset_path
-    ) + '\n'
+    output_builder = DiffOutputBuilder()
+    expected_output = output_builder.build_diff_output(
+        ['c.zip'], [td + '/assets/mods/b.jar']) + '\n'
     assert result.output == expected_output
