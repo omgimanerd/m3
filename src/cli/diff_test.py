f01a7f1f23c90aece9f94276c4fe983648bad827
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-07-15 19:37:43 -0400

    Separated diff evaluation from CLI logic for better testing, rewrote test to validate diff evaluation logic

--- src/cli/diff_test.py
+++ src/cli/diff_test.py
@@ -1,24 +1,13 @@
 """Integration and functional testing for diff.py"""
 
-from unittest.mock import patch
-
-from click.testing import CliRunner
-
-from src.cli.diff import Diff, DiffOutputBuilder
+from src.cli.diff import evaluate_diff
 
 
-@patch('src.config.lockfile.LOCKFILE_FILENAME', 'test_m3.lock.json')
-@patch('src.config.config.CONFIG_FILENAME', 'test_m3.json')
-def test_diff(copy_test_data_directory, current_dir, tmp_path):
+def test_diff(config_from_path, lockfile_from_path, current_dir):
     """Tests that the diff command shows the diff between the lockfile's state
     and the project assets."""
-    ref_path = current_dir / 'testdata/'
-    runner = CliRunner()
-
-    with runner.isolated_filesystem(temp_dir=tmp_path) as td:
-        copy_test_data_directory(ref_path, td)
-        result = runner.invoke(Diff.diff)
-    output_builder = DiffOutputBuilder()
-    expected_output = output_builder.build_diff_output(
-        ['c.zip'], [td + '/assets/mods/b.jar']) + '\n'
-    assert result.output == expected_output
+    config = config_from_path('testdata/test_m3.json')
+    lockfile = lockfile_from_path('testdata/test_m3.lock.json')
+    missing_assets, new_assets = evaluate_diff(config, lockfile)
+    assert missing_assets == ['c.zip']
+    assert new_assets == [current_dir / 'testdata/assets/mods/b.jar']
