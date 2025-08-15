0721ff2da9f0f1b4300c13c5cc4fea12a78f9526
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-08-14 21:58:58 -0400

    Simplify lockfile context manager implementation

--- src/lib/lockfile_context_manager.py
+++ src/lib/lockfile_context_manager.py
@@ -1,27 +1,18 @@
 """Context Managers for m3's lockfile and state on disk."""
 
-from typing import Optional
-
-from src.config.config import Config
 from src.config.loader import load_config_and_lockfile
-from src.config.lockfile import Lockfile
 
 
 class M3ContextManager():
     """Context manager for m3's state on disk."""
 
-    def __init__(self, config: Config, lockfile: Lockfile):
+    def __init__(self):
+        config, lockfile = load_config_and_lockfile()
         self.config = config
         self.lockfile = lockfile
 
-    @staticmethod
-    # pylint: disable-next=missing-function-docstring
-    def create():
-        config, lockfile = load_config_and_lockfile()
-        return M3ContextManager(config, lockfile)
-
-    def __enter__(self) -> tuple[Optional[Config], Optional[Lockfile]]:
-        return (self.config, self.lockfile)
+    def __enter__(self) -> 'M3ContextManager':
+        return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.config.write()
