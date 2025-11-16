725eb4eb3c00b0c36c27561c0379ce8efe5fef41
Ceiran <33044342+Ceiran@users.noreply.github.com>
2025-11-15 21:02:49 -0500

    Make Add and Apply commands atomic

--- src/cli/apply.py
+++ src/cli/apply.py
@@ -1,9 +1,15 @@
 """apply subcommand module"""
 
+import os
+import tempfile
+from pathlib import Path
+
 import click
 
 from src.config.loader import load_config_and_lockfile
 from src.config.lockfile import HASH_ALGS
+from src.lib.copy import copy
+from src.lib.overwrite import overwrite_dir
 from src.util.asset_management import (create_entry_queue, install_assets,
                                        uninstall_assets)
 from src.util.enum import AssetStatus
@@ -28,27 +34,36 @@ class Apply:
         if config is None or lockfile is None:
             raise click.ClickException('Not an m3 project')
 
-        for asset_type, path in config.get_asset_paths().items():
-            lockfile_assets_multikey_dict = lockfile.get_assets_by_type(
-                asset_type)
-            curr_asset_multikey_dict = hash_asset_dir_multi_hash(
-                path, HASH_ALGS)
-            install_queue = create_entry_queue(
-                lockfile_assets_multikey_dict, lockfile_assets_multikey_dict.
-                get_multikey_difference(curr_asset_multikey_dict))
+        with tempfile.TemporaryDirectory() as tmpdir:
+            for asset_type, path in config.get_asset_paths().items():
+                temp_asset_path = Path(tmpdir) / asset_type.value
+                os.mkdir(temp_asset_path)
+                copy(path, Path(temp_asset_path), ['*'], [])
 
-            results = install_assets(install_queue, path)
+                lockfile_assets_multikey_dict = lockfile.get_assets_by_type(
+                    asset_type)
+                curr_asset_multikey_dict = hash_asset_dir_multi_hash(
+                    temp_asset_path, HASH_ALGS)
+                install_queue = create_entry_queue(
+                    lockfile_assets_multikey_dict,
+                    lockfile_assets_multikey_dict.get_multikey_difference(
+                        curr_asset_multikey_dict))
 
-            for i in results[AssetStatus.INSTALLED]:
-                click.echo(f'Installed {i}')
+                results = install_assets(install_queue, Path(temp_asset_path))
 
-            for ni in results[AssetStatus.ERROR_INSTALL]:
-                click.echo(ni)
+                for i in results[AssetStatus.INSTALLED]:
+                    click.echo(f'Installed {i}')
 
-            if remove:
-                uninstall_queue = create_entry_queue(
-                    curr_asset_multikey_dict, curr_asset_multikey_dict.
-                    get_multikey_difference(lockfile_assets_multikey_dict))
-                uninstall_assets(uninstall_queue, path, click.echo)
+                for ni in results[AssetStatus.ERROR_INSTALL]:
+                    click.echo(ni)
+
+                if remove:
+                    uninstall_queue = create_entry_queue(
+                        curr_asset_multikey_dict, curr_asset_multikey_dict.
+                        get_multikey_difference(lockfile_assets_multikey_dict))
+                    uninstall_assets(
+                        uninstall_queue, temp_asset_path, click.echo)
+            for asset_type, path in config.get_asset_paths().items():
+                overwrite_dir(path, (Path(tmpdir) / asset_type.value))
 
         click.echo('Applied lockfile state to development directory')
